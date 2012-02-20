1a86833dabde29ef578217fe79e783cce9c32a3d
Brian Stone <briankstone@gmail.com>
2012-02-20 13:15:22 -0500

    Listen for change events.

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ jQuery Pretext
 Add pretext labels to input fields.
 
     $('input#username').pretext({ text: 'Username' });
+
+### Notes
+
+You need to manually trigger the change event if you are changing the value of
+the input field via JavaScript.
+
+    $('input#username').val('').change();
--- jquery.pretext.js
+++ jquery.pretext.js
@@ -4,33 +4,46 @@
  */
 (function ($) {
     $.fn.pretext = function (options) {
-        var opts = $.extend({ init: true }, options),
-            classname = 'pretext';
+        var opts = $.extend({}, options), classname = 'pretext',
+            methods = {
+                enable: function ($this, password, pretext) {
+                    $this.val(pretext).addClass(classname);
+                    if (password) {
+                        $this.get(0).type = 'text';
+                    }
+                },
+                disable: function ($this, password) {
+                    $this.removeClass(classname);
+                    if (password) {
+                        $this.get(0).type = 'password';
+                    }
+                }
+            };
 
         return this.each(function () {
             var $this = $(this),
                 pretext = opts.text || $this.val(),
                 password = $this.attr('type') === 'password';
 
-            if (opts.init) {
-                $this.addClass(classname).val(pretext).get(0).type = 'text';
-            }
-
-             $this.focus(function () {
-                 if ($this.hasClass(classname)) {
-                     $this.val('').removeClass(classname);
-                     if (password) {
-                         $this.get(0).type = 'password';
-                     }
-                 }
-             }).blur(function () {
-                 if ($this.val() === '') {
-                     $this.val(pretext).addClass(classname);
-                     if (password) {
-                         $this.get(0).type = 'text';
-                     }
-                 };
-             });
+            $this.addClass(classname).val(pretext)
+                .focus(function () {
+                    if ($this.hasClass(classname)) {
+                        $this.val('');
+                        methods.disable($this, password);
+                    }
+                })
+                .blur(function () {
+                    if ($this.val() === '') {
+                        methods.enable($this, password, pretext);
+                    };
+                })
+                .change(function () {
+                    if ($this.val() === '') {
+                        methods.enable($this, password, pretext);
+                    } else {
+                        methods.disable($this, password);
+                    }
+                }).get(0).type = 'text';
         });
     };
 }(jQuery));
