ca8d7fe297e445d88df38f9fa4d6cae60d2eb093
Brian Stone <brstone@cisco.com>
2012-05-01 13:51:20 -0700

    Use the label as an overlay rather than changing the input value.

--- jquery.pretext.js
+++ jquery.pretext.js
@@ -4,64 +4,107 @@
  */
 (function ($) {
 
-    $.Pretext = function (input) {
-        this.input = input;
-        this.$input = $(input);
-        this.$label = $('label[for="' + this.$input.attr('id') + '"]').hide();
-        this.pretext = this.$label.text();
-        this.type = this.$input.attr('type');
+    /**
+     * Add pretext to the given input using the given label.
+     * @param {object} $input The jQuery input element.
+     * @param {object} $label The jQuery label element.
+     * @constructor
+     */
+    $.Pretext = function ($input, $label) {
 
+        /**
+         * The jQuery input element.
+         * @type object
+         */
+        this.$input = $input;
+
+        /**
+         * The jQuery label element.
+         * @type object
+         */
+        this.$label = $label;
+
+        /**
+         * The jQuery element housing the input element.
+         * @type object
+         */
+        this.$container = $(document.createElement('div'));
+
+        // Initialize the pretext object.
         this.init();
     };
 
+    /**
+     * The class name identifying pretext elements.
+     * @type String
+     * @static
+     */
     $.Pretext.CLASSNAME = 'pretext';
 
+    // Extend the prototype
     $.Pretext.prototype = {
 
+        /**
+         * Initialize the pretext object.
+         */
         init: function () {
-            this.$input
-                .focus($.proxy(this.onFocus, this))
-                .blur($.proxy(this.onBlur, this))
-                .change($.proxy(this.onChange, this));
-            this.onBlur();
+
+            // Listen for events that alter the value of the input
+            this.$input.bind('change keydown', $.proxy(this.onChange, this));
+
+            // Initialize the pretext container
+            this.$container
+                .addClass($.Pretext.CLASSNAME + '-container')
+                .css({
+                    position: 'relative',
+                    display: 'inline-block'
+                })
+                .insertAfter(this.$input)
+                .append(this.$input)
+                .append(this.$label);
+
+            // Initialize the pretext label
+            this.$label
+                .addClass($.Pretext.CLASSNAME)
+                .css({
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    cursor: 'text',
+                    color: '#a9a9a9',
+                    'font-family': 'sans-serif',
+                    'font-size': 13.3333
+                });
+
+            // Initialize the pretext state
+            this.onChange();
         },
 
-        onFocus: function () {
-            if (this.$input.hasClass($.Pretext.CLASSNAME)) {
-                this.hide();
-            }
-        },
-
-        onBlur: function () {
-            if (this.$input.val() === '') {
-                this.show();
-            }
-        },
-
+        /**
+         * Handle events that change the value of the input.
+         */
         onChange: function () {
-            if (this.$input.val() === '' && !this.$input.is(':focus')) {
-                this.show();
-            }
-        },
-
-        show: function () {
-            this.$input.val(this.pretext).addClass($.Pretext.CLASSNAME);
-            if (this.type === 'password') {
-                this.input.type = 'text';
-            }
-        },
-
-        hide: function () {
-            this.$input.val('').removeClass($.Pretext.CLASSNAME);
-            if (this.type === 'password') {
-                this.input.type = 'password';
-            }
+            // Add a short delay to allow value to change after keydown
+            setTimeout($.proxy(function () {
+                if (this.$input.val() === '') {
+                    this.$label.show();
+                } else {
+                    this.$label.hide();
+                }
+            }, this), 1);
         }
     };
 
+    // Add "pretext" as a jquery plugin
     $.fn.pretext = function (options) {
         return this.each(function () {
-            (new $.Pretext(this));
+            var $this = $(this),
+                $label = $('label[for="' + $this.attr('id') + '"]');
+
+            // Make sure a label exists and is not already initialized
+            if ($label.length > 0 && !$label.hasClass($.Pretext.CLASSNAME)) {
+                (new $.Pretext($this, $label));
+            }
         });
     };
 
